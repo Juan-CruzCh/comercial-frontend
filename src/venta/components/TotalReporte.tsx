4d3c640c1ab2d6ef6762269ab3b8653c3f8fc220
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-09-10 14:21:21 -0400

    cambios

--- src/venta/components/TotalReporte.tsx
+++ src/venta/components/TotalReporte.tsx
@@ -12,87 +12,105 @@ import {
 import type { ListarVentaI } from "../interface/ventaInterface";
 export const TotalReporte = ({ ventas }: { ventas: ListarVentaI[] }) => {
   const descuentoAcumuladoPorVendedor = ventas.reduce(
-    (acc: Record<string, number>, venta) => {
+    (
+      acc: Record<
+        string,
+        {
+          totalDescuento: number;
+          totalGanancia: number;
+          montoTotal: number;
+          descuento: number;
+          sudTotal: number;
+          totalDescuentoVendedor: number;
+        }
+      >,
+      venta
+    ) => {
       if (!acc[venta.vendedor]) {
-        acc[venta.vendedor] = 0;
+        acc[venta.vendedor] = {
+          descuento: 0,
+          montoTotal: 0,
+          sudTotal: 0,
+          totalDescuento: 0,
+          totalGanancia: 0,
+          totalDescuentoVendedor: 0,
+        };
       }
-      acc[venta.vendedor] += venta.descuentoVendedor;
+      acc[venta.vendedor].totalDescuento += venta.descuentoVendedor;
+      acc[venta.vendedor].sudTotal += venta.subTotal;
+      acc[venta.vendedor].descuento += venta.descuento;
+      acc[venta.vendedor].montoTotal += venta.montoTotal;
+      acc[venta.vendedor].totalGanancia += venta.totalGanancia;
+      acc[venta.vendedor].totalDescuentoVendedor += venta.descuentoVendedor;
       return acc;
     },
     {}
   );
 
-  const descuentoAcumuladoVenededor = Object.entries(descuentoAcumuladoPorVendedor).map(
-    ([vendedor, totalDescuento]) => ({
-      vendedor,
-      totalDescuento,
+  const descuentoAcumuladoVenededor = Object.entries(
+    descuentoAcumuladoPorVendedor
+  ).map(([vendedor, value]) => ({
+    vendedor,
+    totalDescuento: value.totalDescuento,
+    montoTotal: value.montoTotal,
+    descuento: value.descuento,
+    sudTotal: value.sudTotal,
+    totalGanacia: value.totalGanancia,
+  }));
+
+  const descuentoAcumuladoSucursal = ventas.reduce(
+    (
+      acc: Record<
+        string,
+        {
+          totalAlquiler: number;
+          totalGanancia: number;
+          montoTotal: number;
+          descuento: number;
+          sudTotal: number;
+          totalDescuentoVendedor: number;
+        }
+      >,
+      venta
+    ) => {
+      if (!acc[venta.sucursal]) {
+        acc[venta.sucursal] = {
+          totalAlquiler: 0,
+          totalGanancia: 0,
+          montoTotal: 0,
+          descuento: 0,
+          sudTotal: 0,
+          totalDescuentoVendedor: 0,
+        };
+      }
+      acc[venta.sucursal].totalAlquiler += venta.descuentoAlquiller;
+      acc[venta.sucursal].totalGanancia += venta.totalGanancia;
+      acc[venta.sucursal].montoTotal += venta.montoTotal;
+      acc[venta.sucursal].descuento += venta.descuento;
+      acc[venta.sucursal].sudTotal += venta.subTotal;
+      acc[venta.sucursal].totalDescuentoVendedor += venta.descuentoVendedor;
+      return acc;
+    },
+    {}
+  );
+  const acumuladoSucursal = Object.entries(descuentoAcumuladoSucursal).map(
+    ([sucursal, value]) => ({
+      sucursal,
+      totalAcumulado: value.totalAlquiler,
+      totalGanacia: value.totalGanancia,
+      montoTotal: value.montoTotal,
+      descuento: value.descuento,
+      sudTotal: value.sudTotal,
+      totalDescuentoVendedor: value.totalDescuentoVendedor,
     })
   );
 
- const descuentoAcumuladoSucursal=  ventas.reduce((acc:Record<string, number>, venta)=>{
-        if(!acc[venta.sucursal]){
-            acc[venta.sucursal] = 0
-        }
-        acc[venta.sucursal] += venta.descuentoAlquiller
-    return acc
-  },{})
-  const acumuladoSucursal =Object.entries(descuentoAcumuladoSucursal).map(([sucursal, totalAcumulado])=>({sucursal, totalAcumulado}))
-  
   return (
     <Box p={2}>
       <Typography variant="h6" gutterBottom>
         Totales de Ventas
       </Typography>
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Subtotal</TableCell>
-              <TableCell>Descuento</TableCell>
-              <TableCell>Monto Total</TableCell>
-              <TableCell>Total Alquiler</TableCell>
-              <TableCell>Total Vendedor</TableCell>
-              <TableCell>Total Acumulado</TableCell>
-              <TableCell>Total Ganancia</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell>
-                {ventas.reduce((acc, i) => i.subTotal + acc, 0).toFixed(2)} Bs
-              </TableCell>
-              <TableCell>
-                {ventas.reduce((acc, i) => i.descuento + acc, 0).toFixed(2)} Bs
-              </TableCell>
-              <TableCell>
-                {ventas.reduce((acc, i) => i.montoTotal + acc, 0).toFixed(2)} Bs
-              </TableCell>
-              <TableCell>
-                {ventas
-                  .reduce((acc, i) => i.descuentoAlquiller + acc, 0)
-                  .toFixed(2)}{" "}
-                Bs
-              </TableCell>
-              <TableCell>
-                {ventas
-                  .reduce((acc, i) => i.descuentoVendedor + acc, 0)
-                  .toFixed(2)}{" "}
-                Bs
-              </TableCell>
-              <TableCell>
-                {ventas
-                  .reduce((acc, i) => i.descuentoAcumulado + acc, 0)
-                  .toFixed(2)}{" "}
-                Bs
-              </TableCell>
-              <TableCell>
-                {ventas.reduce((acc, i) => i.totalGanancia + acc, 0).toFixed(2)}{" "}
-                Bs
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+
       <Box mt={4}>
         <Typography variant="h6" gutterBottom>
           Descuento acumulado por sucursal
@@ -102,18 +120,29 @@ export const TotalReporte = ({ ventas }: { ventas: ListarVentaI[] }) => {
             <TableHead>
               <TableRow>
                 <TableCell>Vendedor</TableCell>
-                <TableCell>Descuento Total</TableCell>
+                <TableCell>acumulado vendedor</TableCell>
+                <TableCell>acumulado alquiler</TableCell>
+                <TableCell>SudTotal</TableCell>
+                <TableCell>descuento</TableCell>
+                <TableCell>Monto</TableCell>
+                <TableCell>Total Ganancia</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {/* Ejemplo de fila, puedes repetir según el número de vendedores */}
-               {acumuladoSucursal.map((item) => (
+              {acumuladoSucursal.map((item) => (
                 <TableRow>
                   <TableCell>{item.sucursal}</TableCell>
-                  <TableCell>{item.totalAcumulado.toFixed(2)}</TableCell>
+                  <TableCell>
+                    {item.totalDescuentoVendedor.toFixed(2)} Bs
+                  </TableCell>
+                  <TableCell>{item.totalAcumulado.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.sudTotal.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.descuento.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.montoTotal.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.totalGanacia.toFixed(2)} Bs</TableCell>
                 </TableRow>
               ))}
-             
             </TableBody>
           </Table>
         </TableContainer>
@@ -127,14 +156,22 @@ export const TotalReporte = ({ ventas }: { ventas: ListarVentaI[] }) => {
             <TableHead>
               <TableRow>
                 <TableCell>Vendedor</TableCell>
-                <TableCell>Descuento acumulado Total</TableCell>
+                <TableCell>acumulado para vendedor</TableCell>
+                <TableCell>SudTotal</TableCell>
+                <TableCell>descuento</TableCell>
+                <TableCell>Monto</TableCell>
+                <TableCell>Total Ganancia</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
               {descuentoAcumuladoVenededor.map((item) => (
                 <TableRow>
                   <TableCell>{item.vendedor}</TableCell>
-                  <TableCell>{item.totalDescuento.toFixed(2)}</TableCell>
+                  <TableCell>{item.totalDescuento.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.sudTotal.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.descuento.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.montoTotal.toFixed(2)} Bs</TableCell>
+                  <TableCell>{item.totalGanacia.toFixed(2)} Bs</TableCell>
                 </TableRow>
               ))}
             </TableBody>
